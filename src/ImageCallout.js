f80f9fec96c89a9ae85c8cce5005f47180152531
Gerald-Star <chimagerald3@gmail.com>
2022-12-29 10:17:09 +0100

    commit: added callout component

---
+++ src/ImageCallout.js
@@ -0,0 +1,16 @@
+
+
+// import React from "react"
+// import './ImageCallout.css'
+
+// function ImageCallout(props) {
+//     return (
+//         <div className="callout">
+//             <img src={props.img} alt="pictures" width="100%" />
+
+//             <figcaption>{props.caption}</figcaption>
+//         </div>
+//     )
+// }
+
+// export default ImageCallout
---
+++ src/InfoCallout.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+
+function InfoCallout(props) {
+    return (
+        <div className="callout">
+            <h2>{props.header}</h2>
+            <p>{props.body}</p>
+        </div>
+    )
+}
+
+export default InfoCallout
